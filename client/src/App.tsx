3a3f1d50f71fafefd3156873102425796a1ede28
mislamfullstackdev <mislam.pasona@gmail.com>
2024-10-07 18:23:12 +0900

    Fix error and added tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
+//import './index.css';
 
 import AddTodo from './components/AddTodo';
 import TodoItems from './components/TodoItems';
